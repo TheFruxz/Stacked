7c96eff562b1ab5991ba2f62f6f47b363656abc2
TheFruxz <cedricspitzer@outlook.de>
2022-10-02 09:39:05 +0200

    feature: improved component joining (now like strings)

--- src/main/kotlin/de/fruxz/stacked/extension/ComponentCollection.kt
+++ src/main/kotlin/de/fruxz/stacked/extension/ComponentCollection.kt
@@ -1,12 +1,45 @@
 package de.fruxz.stacked.extension
 
 import de.fruxz.ascend.extension.container.splitBy
+import de.fruxz.stacked.buildComponent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.ComponentLike
 import net.kyori.adventure.text.TextComponent
 
-fun Collection<ComponentLike>.joinToComponent(): TextComponent =
+/**
+ * This function creates a [TextComponent] containing
+ * all components of [this] [Iterable] of [ComponentLike]s.
+ * No spliterator is used!
+ * @author Fruxz
+ * @since 1.0
+ */
+fun Iterable<ComponentLike>.joinToComponent(): TextComponent =
     Component.text().append(this).build()
+
+/**
+ * This function creates a [TextComponent] containing
+ * all components of [this] [Iterable] of [ComponentLike]s
+ * split by the specified [spliterator].
+ * @author Fruxz
+ * @since 1.0
+ */
+fun Iterable<ComponentLike>.joinToComponent(spliterator: ComponentLike): TextComponent =
+    joinToComponent(spliterator) { it }
+
+/**
+ * This function creates a [TextComponent] containing
+ * all components of [this] [Iterable] of [ComponentLike]s,
+ * transformed via the [builder] process and split by the
+ * specified [spliterator].
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <T : ComponentLike> Iterable<T>.joinToComponent(spliterator: ComponentLike, builder: (T) -> ComponentLike): TextComponent = buildComponent {
+    withIndex().forEach { (index, split) ->
+        if (index > 0) append(spliterator)
+        append(builder.invoke(split))
+    }
+}
 
 /**
  * This function splits every line of the component to its
