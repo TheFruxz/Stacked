bdd0c9b8a47c9a715d860b2190f2e57c2dd7b939
TheFruxz <cedricspitzer@outlook.de>
2022-10-22 10:40:14 +0200

    fix: return of builder creates confusion and relates to duplications

--- src/main/kotlin/de/fruxz/stacked/extension/Builder.kt
+++ src/main/kotlin/de/fruxz/stacked/extension/Builder.kt
@@ -1,17 +1,18 @@
 package de.fruxz.stacked.extension
 
+import de.fruxz.ascend.extension.dump
 import de.fruxz.stacked.Stacked
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 
 @Stacked
-fun TextComponent.Builder.newlines(amount: Int) = apply { repeat(amount) { append(Component.newline()) } }
+fun TextComponent.Builder.newlines(amount: Int) = apply { repeat(amount) { append(Component.newline()) } }.dump()
 
 @Stacked
-fun TextComponent.Builder.newline() = apply { append(Component.newline()) }
+fun TextComponent.Builder.newline() = apply { append(Component.newline()) }.dump()
 
 @Stacked
-fun TextComponent.Builder.space() = append(Component.space())
+fun TextComponent.Builder.space() = append(Component.space()).dump()
 
 @Stacked
-fun TextComponent.Builder.spaces(amount: Int) = apply { repeat(amount) { space() } }
+fun TextComponent.Builder.spaces(amount: Int) = apply { repeat(amount) { space() } }.dump()
