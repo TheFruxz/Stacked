e6488b1e3e870823524e24f18c26c2f1ff12347a
Fruxz <cedricspitzer@outlook.de>
2023-06-27 18:54:59 +0200

    chore: update Kotlin to 1.9.0-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion.VERSION_17
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.0-RC"
     kotlin("plugin.serialization") version "1.8.22"
     id("org.jetbrains.dokka") version "1.8.20"
     `maven-publish`
