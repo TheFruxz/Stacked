91acb7b9103a1d829769174ffed21d4904580473
Fruxz <cedricspitzer@outlook.de>
2023-10-31 16:22:43 +0100

    chore: add experimental annotation

---
+++ src/main/kotlin/dev/fruxz/stacked/extension/api/StackedExperimental.kt
@@ -0,0 +1,5 @@
+package dev.fruxz.stacked.extension.api
+
+@MustBeDocumented
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR, message = "This utilizes experimental features, stay safe!")
+internal annotation class StackedExperimental
