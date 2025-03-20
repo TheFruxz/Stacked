d798b6746158c170beb03ef59b7cac47a8959d9d
TheFruxz <cedricspitzer@outlook.de>
2025-03-20 13:26:14 +0100

    chore: update kotlin to 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.0.21"
     id("org.jetbrains.dokka") version "1.9.20"
     `maven-publish`
