f69aae1c0c0e0747844c65b83861c80dff7e8420
TheFruxz <cedricspitzer@outlook.de>
2025-03-20 16:14:26 +0100

    chore: remove unnused dokka tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,23 +32,6 @@ dependencies {
 
 }
 
-val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
-    dependsOn(tasks.dokkaJavadoc)
-    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
-    archiveClassifier.set("javadoc")
-}
-
-val dokkaHtmlJar by tasks.register<Jar>("dokkaHtmlJar") {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
-    archiveClassifier.set("html-doc")
-}
-
-val sourceJar by tasks.register<Jar>("sourceJar") {
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
-}
-
 publishing {
 
     repositories {
