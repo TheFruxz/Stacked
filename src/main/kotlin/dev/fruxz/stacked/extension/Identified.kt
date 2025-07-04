b50b3128d7a6adea21e669f797bbf8caf9e53b15
TheFruxz <cedricspitzer@outlook.de>
2025-07-04 17:20:41 +0200

    feature: add identifiesNot function
    as the negative of the identifies function

--- src/main/kotlin/dev/fruxz/stacked/extension/Identified.kt
+++ src/main/kotlin/dev/fruxz/stacked/extension/Identified.kt
@@ -14,3 +14,17 @@ import net.kyori.adventure.identity.Identified
  */
 infix fun Identified.identifies(other: Identified): Boolean =
     this.identity().uuid() == other.identity().uuid()
+
+/**
+ * This function checks if the current [Identified] instance does not identify the [other] [Identified] instance.
+ * This means both instances do not have the same [java.util.UUID] behind their [Identified.identity].
+ * @param other The [Identified] instance to compare with.
+ * @return `true` if both instances do not have the same UUID, otherwise `false`.
+ * @see Identified.identity
+ * @see net.kyori.adventure.identity.Identity.uuid
+ * @see Identified.identifies
+ * @author Fruxz
+ * @since 2025.7
+ */
+infix fun Identified.identifiesNot(other: Identified): Boolean =
+    !this.identifies(other)
