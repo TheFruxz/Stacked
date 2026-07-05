04ba6ba5f4a98190867ae8b27557b280323cb1be
TheFruxz <28064149+TheFruxz@users.noreply.github.com>
2026-07-06 01:43:01 +0200

    chore: remove kotlin-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     kotlin("jvm") version "2.4.0"
     kotlin("plugin.serialization") version "2.4.0"
     id("org.jetbrains.dokka") version "2.2.0"
-    id("org.hildan.kotlin-publish") version "1.7.0"
     `maven-publish`
 }
 
